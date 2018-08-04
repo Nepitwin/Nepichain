db250e8404870acd99f9e5d1df2a27037782a86d
Andreas Sekulski <andreas.sekulski@gmail.com>
2018-08-04 21:28:07 +0200

    Documentation from string util

--- src/main/java/com/asekulsk/nepichain/util/StringUtil.java
+++ src/main/java/com/asekulsk/nepichain/util/StringUtil.java
@@ -3,11 +3,19 @@ package com.asekulsk.nepichain.util;
 import java.security.*;
 import java.util.Base64;
 
+/**
+ * String utility helper class.
+ * @author Andreas Sekulski
+ */
 public class StringUtil {
 
+    /**
+     * Get string representation from key.
+     * @param key Key to convert as String.
+     * @return String representation from key.
+     */
     public static String getStringFromKey(Key key) {
         return Base64.getEncoder().encodeToString(key.getEncoded());
     }
-
 }
 
