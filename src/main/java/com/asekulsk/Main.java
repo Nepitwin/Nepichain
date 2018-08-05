a6d33c12c42fdd715d9f136d25ec5752245a6ce7
Andreas Sekulski <andreas.sekulski@gmail.com>
2018-08-05 12:28:26 +0200

    Chain class implementation for code cleanup

--- src/main/java/com/asekulsk/Main.java
+++ src/main/java/com/asekulsk/Main.java
@@ -1,12 +1,12 @@
 package com.asekulsk;
 
 import com.asekulsk.nepichain.client.model.blockchain.Block;
+import com.asekulsk.nepichain.client.model.blockchain.Chain;
+import com.asekulsk.nepichain.client.model.blockchain.Message;
 import com.asekulsk.nepichain.client.model.security.Mailbox;
-import com.google.gson.GsonBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.security.Security;
-import java.util.ArrayList;
 
 /**
  * Example for Nepichain as messaging mail encryption or decryption service.
@@ -24,86 +24,40 @@ import java.util.ArrayList;
  */
 public class Main {
 
-    /**
-     * Jack Bauers Mailbox blockchain
-     */
-    public static ArrayList<Block> blockchain_jack_bauer = new ArrayList<Block>();
-
-    public static ArrayList<Block> blockchain_max_mustersecurity = new ArrayList<Block>();
-
     public static void main(String[] args) {
-        //Setup Bouncy castle as a Security Provider
+
+        // Setup Bouncy castle as a Security Provider
         Security.addProvider(new BouncyCastleProvider());
 
         Mailbox mailbox_max_mustersecurity = new Mailbox("max_mustersecurity@com.de", "Max Mustersecurity");
         Mailbox mailbox_jack_bauer = new Mailbox("jack_bauer@24.de", "Jack Bauer");
 
-        // ToDo := This could be nicer to make a java class blockchain...
-        // Send e-mail messages to jack ;)
-        blockchain_jack_bauer.add(
-                new Block(
-                        mailbox_max_mustersecurity.createMessageForMailbox(mailbox_jack_bauer.getPublicKey(),
-                                "Hello Jack Bauer you got 24 hours to stop me ;)"),
-                        "0"));
+        // Blockchain from Jack Bauer to obtain messages
+        Chain chain_jack_bauer = new Chain(mailbox_jack_bauer.getFullName());
 
-        blockchain_jack_bauer.add(
-                new Block(
-                        mailbox_max_mustersecurity.createMessageForMailbox(mailbox_jack_bauer.getPublicKey(),
-                                "Hello Jack Bauer you got 23 hours to stop me ;)"),
-                        blockchain_jack_bauer.get(blockchain_jack_bauer.size()-1).getHash()));
+        Message message = mailbox_max_mustersecurity.createMessageForMailbox(mailbox_jack_bauer.getPublicKey(),
+                "Hello Jack Bauer you got 24 hours to stop me ;)");
+        chain_jack_bauer.addBlockToChain(new Block(message, chain_jack_bauer.getLastHash()));
 
-        blockchain_jack_bauer.add(
-                new Block(
-                        mailbox_max_mustersecurity.createMessageForMailbox(mailbox_jack_bauer.getPublicKey(),
-                                "Hello Jack the bomb is ticking ;)"),
-                        blockchain_jack_bauer.get(blockchain_jack_bauer.size()-1).getHash()));
+        message = mailbox_max_mustersecurity.createMessageForMailbox(mailbox_jack_bauer.getPublicKey(),
+                "Hello Jack Bauer you got 23 hours to stop me ;)");
+        chain_jack_bauer.addBlockToChain(new Block(message, chain_jack_bauer.getLastHash()));
+
+        message = mailbox_max_mustersecurity.createMessageForMailbox(mailbox_jack_bauer.getPublicKey(),
+                "Hello Jack the bomb is ticking ;)");
+        chain_jack_bauer.addBlockToChain(new Block(message, chain_jack_bauer.getLastHash()));
 
         // Blockchain validation
-        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain_jack_bauer);
-        System.out.println(blockchainJson);
-        System.out.println("Chain is " + (isChainValid() ? "valid" : "not valid"));
-        System.out.println();
-
-        //Verify the signature works and verify it from the public key
-        System.out.println("Is signature verified :" + blockchain_jack_bauer.get(0).getData().verifySignature());
+        System.out.println(chain_jack_bauer.toJSON());
+        System.out.println("Chain is " + (chain_jack_bauer.isChainValid() ? "valid" : "not valid"));
         System.out.println();
 
         // Decrypt messages for jack
-        System.out.println(mailbox_jack_bauer.getMessageFromBlock(blockchain_jack_bauer.get(0).getData()));
-        System.out.println(mailbox_jack_bauer.getMessageFromBlock(blockchain_jack_bauer.get(1).getData()));
-        System.out.println(mailbox_jack_bauer.getMessageFromBlock(blockchain_jack_bauer.get(2).getData()));
-        System.out.println();
-    }
-
-    public static Boolean isChainValid() {
-        Block currentBlock;
-        Block previousBlock;
-
-        //loop through blockchain to check hashes:
-        for(int i=1; i < blockchain_jack_bauer.size(); i++) {
-            currentBlock = blockchain_jack_bauer.get(i);
-            previousBlock = blockchain_jack_bauer.get(i-1);
-
-            // compare registered hash and calculated hash:
-            // Check hash from next block
-            if(!currentBlock.getHash().equals(currentBlock.calculateHash()) ){
-                System.out.println("Current Hashes not equal");
-                return false;
-            }
-
-            //compare previous hash and registered previous hash
-            if(!previousBlock.getHash().equals(currentBlock.getPreviousHash()) ) {
-                System.out.println("Previous Hashes not equal");
-                return false;
-            }
-
-            // ToDo := Andy FIX := I modified first mail and chain validation will be true if not checked first chain element because algorithm don't check hash calculation from first element
-            //check if previous block not modified
-            if(!previousBlock.getHash().equals(previousBlock.calculateHash()) ) {
-                System.out.println("Hash block modified");
-                return false;
-            }
+        for(int i = 0; i < chain_jack_bauer.size(); i++) {
+            System.out.println("Is signature verified : " + chain_jack_bauer.getMessage(i).verifySignature());
+            System.out.println(mailbox_jack_bauer.getMessageFromBlock(chain_jack_bauer.getMessage(i)));
+            System.out.println();
         }
-        return true;
+
     }
 }
---
+++ src/main/java/com/asekulsk/nepichain/client/model/blockchain/Chain.java
@@ -0,0 +1,117 @@
+package com.asekulsk.nepichain.client.model.blockchain;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+/**
+ * Chain class to store all Blocks from messaging.
+ * @author Andreas Sekulski
+ */
+public class Chain {
+
+    /**
+     * Blockchain to store block messages to user.
+     */
+    private ArrayList<Block> blockchain;
+
+    /**
+     * Owner from chain to send messages.
+     */
+    private String owner;
+
+    /**
+     * GSON to serialize json data.
+     */
+    private Gson gson;
+
+    /**
+     * Constructor to create blockchain for a given owner.
+     * @param owner Owner from blockchain.
+     */
+    public Chain(String owner) {
+        this.blockchain = new ArrayList<>();
+        this.gson = new Gson();
+        this.owner = owner;
+    }
+
+    /**
+     * Add block to chain.
+     * @param block Block to add.
+     * @return TRUE if block is added otherwise FALSE.
+     */
+    public boolean addBlockToChain(Block block) {
+        return block != null && blockchain.add(block);
+    }
+
+    /**
+     * Get last hash value from blockchain.
+     * @return If blockchain is empty "0" will be returned otherwise actual hash from last block.
+     */
+    public String getLastHash() {
+        return blockchain.isEmpty() ? "0" : blockchain.get(blockchain.size() - 1).getHash();
+    }
+
+    /**
+     * Generates blockchain as json.
+     * @return JSON string from blockchain.
+     */
+    public String toJSON() {
+        return gson.toJson(blockchain);
+    }
+
+    /**
+     * Get message from blockchain.
+     * @param index Index from message to obtain.
+     * @return NULL if index greater or smaller than blockchain size otherwise message.
+     */
+    public Message getMessage(int index) {
+        return index < size() && index >= 0 ? blockchain.get(index).getData() : null;
+    }
+
+    /**
+     * Size from blockchain.
+     * @return Size from blockchain as integer.
+     */
+    public int size() {
+        return blockchain.size();
+    }
+
+    /**
+     * Verification if chain still valid and not manipulated.
+     * @return TRUE if chain is valid otherwise false.
+     */
+    public Boolean isChainValid() {
+
+        Block currentBlock;
+        Block previousBlock;
+
+        //loop through blockchain to check hashes:
+        for(int i=1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+
+            // compare registered hash and calculated hash:
+            // Check hash from next block
+            if(!currentBlock.getHash().equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+
+            //compare previous hash and registered previous hash
+            if(!previousBlock.getHash().equals(currentBlock.getPreviousHash()) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+
+            // Andy FIX := I modified first mail and chain validation will be true if not checked first chain element because algorithm don't check hash calculation from first element.
+            // For immutable model classes not needed.
+            //check if previous block not modified.
+            if(!previousBlock.getHash().equals(previousBlock.calculateHash()) ) {
+                System.out.println("Hash block modified");
+                return false;
+            }
+        }
+        return true;
+    }
+}
